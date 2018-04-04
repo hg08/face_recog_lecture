23df9ae244f52e89dac9d9eef204734d41669038
Huang Gang <huangjiadong08@yeah.net>
2018-04-04 17:39:09 +0800

    180404

--- face_recog_steps/step1-find_faces.py
+++ face_recog_steps/step1-find_faces.py
@@ -1,36 +1,39 @@
+#coding:utf-8
 import sys
 import dlib
 from skimage import io
 
-# Take the image file name from the command line
+# 从命令行参数获取图像名称file_name
 file_name = sys.argv[1]
 
-# Create a HOG face detector using the built-in dlib class
+# 建立一个HOG人脸探测器
+# 方法：用内置的dlib类
 face_detector = dlib.get_frontal_face_detector()
 
+#建立窗口对象
 win = dlib.image_window()
 
-# Load the image into an array
+# 将图片存进一个数组
 image = io.imread(file_name)
 
-# Run the HOG face detector on the image data.
-# The result will be the bounding boxes of the faces in our image.
+#在图片数据上运行HOG人脸探测器.
+# 结果：人脸边界盒子.
 detected_faces = face_detector(image, 1)
 
-print("I found {} faces in the file {}".format(len(detected_faces), file_name))
+print("发现{}张人脸于文件{}".format(len(detected_faces), file_name))
 
-# Open a window on the desktop showing the image
+# 在桌面打开一个窗口以显示图片
 win.set_image(image)
 
-# Loop through each face we found in the image
+# 遍历照片中的每一张人脸
 for i, face_rect in enumerate(detected_faces):
 
     # Detected faces are returned as an object with the coordinates
     # of the top, left, right and bottom edges
-    print("- Face #{} found at Left: {} Top: {} Right: {} Bottom: {}".format(i, face_rect.left(), face_rect.top(), face_rect.right(), face_rect.bottom()))
+    print("- 发现人脸#{}, 位置: {} Top: {} Right: {} Bottom: {}".format(i, face_rect.left(), face_rect.top(), face_rect.right(), face_rect.bottom()))
 
-    # Draw a box around each face we found
+    # 在发现的每张人脸周围画一个盒子
     win.add_overlay(face_rect)
 
-# Wait until the user hits <enter> to close the window
+# 等待，当用户输入回车键时关闭显示图片的窗口
 dlib.hit_enter_to_continue()
